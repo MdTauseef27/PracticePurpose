4162840cb03b36ed9404a61523962a58fec7116e
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-10-15 14:32:40 +0530

    hiiiiii

--- src/Component/Tab2.jsx
+++ src/Component/Tab2.jsx
@@ -11,6 +11,8 @@ export const Tab2=() =>
     <h1 >Hello Friends</h1>
     <h2 className="primary">hiiiiiiiiiiiii</h2>
     <h3 id="heading">byeeeeeeeeee</h3>
+    <h4 className='header'>React</h4>
+    <p className='header'>Well Come To The world Of React</p>
     </div>
   )
  
