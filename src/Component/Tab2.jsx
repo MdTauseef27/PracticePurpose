2133a3d050011c7ed32e2cc30b6385569af21d05
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-10-16 14:59:34 +0530

    button added

--- src/Component/Tab2.jsx
+++ src/Component/Tab2.jsx
@@ -1,37 +1,32 @@
 import React from "react";
-import './Stylesheet.css';
-import {Row,Col,Container} from 'react-bootstrap'; 
-export const Tab2=() =>
-{
+import "./Stylesheet.css";
+import { Row, Col, Container } from "react-bootstrap";
+export const Tab2 = () => {
   return (
-
     <div>
-      
-
       <Container fluid>
         <Row>
           <Col sm={2}>
-    <h1 >Hello Friends</h1>
-    </Col>
-    <Col sm={2}>
-    <h2 className="primary" >hiiiiiiiiiiiii</h2>
-    </Col>
-    <Col sm={2}>
-    <h3 id="heading">byeeeeeeeeee</h3>
-    </Col>
-    <Col sm={2}>
-    <h4 className='header'>React</h4>
-    </Col>
-    <Col sm={2}>
-    <p className='header'>Well Come To The world Of React</p>
-    </Col>
-    <Col sm={2}>
-    <p>okkkkkkkkkk</p>
-    </Col>
-    </Row>
-    </Container>
+            <h1>Hello Friends</h1>
+          </Col>
+          <Col sm={2}>
+            <h2 className="primary">hiiiiiiiiiiiii</h2>
+          </Col>
+          <Col sm={2}>
+            <h3 id="heading">byeeeeeeeeee</h3>
+          </Col>
+          <Col sm={2}>
+            <h4 className="header">React</h4>
+          </Col>
+          <Col sm={2}>
+            <p className="header">Well Come To The world Of React</p>
+          </Col>
+          <Col sm={2}>
+            <p>okkkkkkkkkk</p>
+            <Button>Submit</Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
-  )
- 
+  );
 };
-
