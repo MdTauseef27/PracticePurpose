7e5e1fd9a30c8cd08aee2b3552a6403a87bedbf7
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-10-29 10:07:07 +0530

    card added

--- src/Component/Cardex.jsx
+++ src/Component/Cardex.jsx
@@ -5,53 +5,74 @@ import "../Component/Stylesheetex.css";
 export const Cardex = () => {
   return (
     <div>
-      <Container fluid>
-        <Card
-          style={{
-            width: "25rem",
-            height: "8rem",
-            border: "1px solid black",
-            marginTop: "5px",
-          }}
-        >
-          <Row>
-            <Col sm={3}>
-              <Card.Img src={ImgLogo} className="avatar" alt="" />
-            </Col>
-            <Col sm={9}>
-              <Card.Body style={{ margin: "10px" }}>
-                <Card.Title>Features Of React</Card.Title>
-                <Card.Text>
-                  React is a open source library created by the facebook.
-                </Card.Text>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card>
+      <Card
+        style={{
+          width: "25rem",
+          height: "8rem",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <Row>
+          <Col sm={3}>
+            <Card.Img src={ImgLogo} className="avatar" alt="" />
+          </Col>
+          <Col sm={9}>
+            <Card.Body style={{ margin: "10px" }}>
+              <Card.Title>Features Of React</Card.Title>
+              <Card.Text>
+                React is a open source library created by the facebook.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
 
-        <Card
-          style={{
-            width: "25rem",
-            height: "8rem",
-            border: "1px solid black",
-            marginTop: "5px",
-          }}
-        >
-          <Row>
-            <Col sm={3}>
-              <Card.Img src={ImgLogo} className="avatar" alt="" />
-            </Col>
-            <Col sm={9}>
-              <Card.Body style={{ margin: "10px" }}>
-                <Card.Title>Features Of React</Card.Title>
-                <Card.Text>
-                  React is a open source library created by the facebook.
-                </Card.Text>
-              </Card.Body>
-            </Col>
-          </Row>
-        </Card>
-      </Container>
+      <Card
+        style={{
+          width: "25rem",
+          height: "8rem",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <Row>
+          <Col sm={3}>
+            <Card.Img src={ImgLogo} className="avatar" alt="" />
+          </Col>
+          <Col sm={9}>
+            <Card.Body style={{ margin: "10px" }}>
+              <Card.Title>Features Of React</Card.Title>
+              <Card.Text>
+                React is a open source library created by the facebook.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+
+      <Card
+        style={{
+          width: "25rem",
+          height: "8rem",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <Row>
+          <Col sm={3}>
+            <Card.Img src={ImgLogo} className="avatar" alt="" />
+          </Col>
+          <Col sm={9}>
+            <Card.Body style={{ margin: "10px" }}>
+              <Card.Title>Features Of React</Card.Title>
+              <Card.Text>
+                React is a open source library created by the facebook.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
     </div>
   );
 };
