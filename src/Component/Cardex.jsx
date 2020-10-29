ce4a3559e0faa8f2c436b43bddcf2a2969e14fe4
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-10-29 09:54:06 +0530

    added card

--- src/Component/Cardex.jsx
+++ src/Component/Cardex.jsx
@@ -4,27 +4,52 @@ import ImgLogo from "./Img/main1.jpg";
 import "../Component/Stylesheetex.css";
 export const Cardex = () => {
   return (
-    <Card
-      style={{
-        width: "25rem",
-        height: "8rem",
-        border: "1px solid black",
-        marginTop: "5px",
-      }}
-    >
-      <Row>
-        <Col sm={3}>
-          <Card.Img src={ImgLogo} className="avatar" alt="" />
-        </Col>
-        <Col sm={9}>
-          <Card.Body style={{ margin: "10px" }}>
-            <Card.Title>Features Of React</Card.Title>
-            <Card.Text>
-              React is a open source library created by the facebook.
-            </Card.Text>
-          </Card.Body>
-        </Col>
-      </Row>
-    </Card>
+    <div>
+      <Card
+        style={{
+          width: "25rem",
+          height: "8rem",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <Row>
+          <Col sm={3}>
+            <Card.Img src={ImgLogo} className="avatar" alt="" />
+          </Col>
+          <Col sm={9}>
+            <Card.Body style={{ margin: "10px" }}>
+              <Card.Title>Features Of React</Card.Title>
+              <Card.Text>
+                React is a open source library created by the facebook.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+
+      <Card
+        style={{
+          width: "25rem",
+          height: "8rem",
+          border: "1px solid black",
+          marginTop: "5px",
+        }}
+      >
+        <Row>
+          <Col sm={3}>
+            <Card.Img src={ImgLogo} className="avatar" alt="" />
+          </Col>
+          <Col sm={9}>
+            <Card.Body style={{ margin: "10px" }}>
+              <Card.Title>Features Of React</Card.Title>
+              <Card.Text>
+                React is a open source library created by the facebook.
+              </Card.Text>
+            </Card.Body>
+          </Col>
+        </Row>
+      </Card>
+    </div>
   );
 };
