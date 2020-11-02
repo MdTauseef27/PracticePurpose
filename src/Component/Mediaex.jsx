87ee7e525c44e9019fbf6addd6a6aa760088406a
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-02 09:47:12 +0530

    removes submit button

--- src/Component/Mediaex.jsx
+++ src/Component/Mediaex.jsx
@@ -76,7 +76,6 @@ export const Mediaex = () => {
           <img src={CommentLogo} alt=""></img>
           <img src={PinLogo} alt=""></img>
           <img src={LikesLogo} alt=""></img>
-          <button>Submit</button>
         </Media.Body>
       </Media>
     </div>
