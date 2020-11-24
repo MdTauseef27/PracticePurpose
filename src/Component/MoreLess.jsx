7172fe1690f372b89d280d29c96c9f73718619d3
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-24 09:34:03 +0530

    small changes

--- src/Component/MoreLess.jsx
+++ src/Component/MoreLess.jsx
@@ -2,6 +2,6 @@ import React, { useState } from "react";
 export const MoreLess = () => {
   const MAX_ITEMS = 2;
   const [isOpen, setIsOpen] = useState(false);
-  const items = ["Item1", "Item2", "Item3"];
+  const items = ["Item1", "Item2", "Item3", "Item4"];
   return <div></div>;
 };
