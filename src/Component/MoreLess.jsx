5118cb959b051fda48a0a6bf731d35e2ccdeab9e
MdTauseef27 <71385149+MdTauseef27@users.noreply.github.com>
2020-11-25 09:15:32 +0530

    toggle function

--- src/Component/MoreLess.jsx
+++ src/Component/MoreLess.jsx
@@ -3,6 +3,8 @@ export const MoreLess = () => {
   const MAX_ITEMS = 2;
   const [isOpen, setIsOpen] = useState(false);
   const items = ["Item1", "Item2", "Item3", "Item4", "Item5", "Item6"];
-  const toggle = () => {};
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
   return <div></div>;
 };
